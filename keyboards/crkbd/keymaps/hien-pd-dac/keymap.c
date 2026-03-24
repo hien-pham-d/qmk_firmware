edc22f87d551b7f6b19c5cdfd3ce262ea2fe3081
hien-pham-d <dh.pham.dev@gmail.com>
2026-03-25 07:10:48 +0900

    chore: update keymap.c ctrl, esc, tab

--- keyboards/crkbd/keymaps/hien-pd-dac/keymap.c
+++ keyboards/crkbd/keymaps/hien-pd-dac/keymap.c
@@ -29,9 +29,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_L] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB, LALT_T(KC_A), LSFT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G,    KC_H, RCTL_T(KC_J), RGUI_T(KC_K), RSFT_T(KC_L), RALT_T(KC_QUOT), KC_ENT,
+       KC_LCTL, LALT_T(KC_A), LSFT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G,    KC_H, RCTL_T(KC_J), RGUI_T(KC_K), RSFT_T(KC_L), RALT_T(KC_SCLN), KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BTN1,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_TRNS,   KC_TRNS,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_ENT, KC_SPC, KC_BSPC, KC_BTN1,  KC_BTN2,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_TRNS,   KC_TRNS,
+      KC_ESC,  KC_ENT, KC_SPC, KC_BSPC, KC_BTN1,  KC_BTN2,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_TRNS,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS,   KC_LT,   KC_GT, KC_QUES,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS, KC_TRNS, KC_TILD, KC_EQL, KC_DQUO, KC_UNDS,                      KC_PIPE, KC_MINS, KC_COLN, KC_LCBR, KC_RCBR,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS,  KC_GRV, KC_LPRN, KC_RPRN, KC_QUES,                      KC_BSLS, KC_PLUS, KC_SCLN, KC_LBRC, KC_RBRC,   KC_TRNS,
+      KC_TRNS, KC_TRNS,  KC_GRV, KC_LPRN, KC_RPRN, KC_QUES,                      KC_BSLS, KC_PLUS, KC_QUOT, KC_LBRC, KC_RBRC,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,   MO(_ADJUST_L), KC_TRNS,    KC_TRNS, KC_TRNS,   KC_TRNS
                                       //`--------------------------'  `--------------------------'
